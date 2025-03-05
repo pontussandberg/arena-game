f4be0d6978656bcc95fefcaead22aac162ff53f7
Pontus Sandberg <sandbergpontus97@gmail.com>
2025-03-05 23:31:55 +0100

    Asset

--- src/objects/index.ts
+++ src/objects/index.ts
@@ -1,3 +1,6 @@
 export * from "./Platform";
 export * from "./Player";
 export * from "./FullscreenBtn";
+export * from "./FloatingBar";
+export * from "./CooldownBar";
+export * from "./HealthBar";
